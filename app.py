2a54e24e21c2c5f20dc95c28f34e50c9e28c6a4a
csptuts <85820742+csptuts@users.noreply.github.com>
2024-08-27 22:48:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,67 +7,23 @@ import pandas as pd
 
 # df
 
-if 'stdt' not in st.session_state:
-    st.session_state['stdt'] = 0
-
-if 'eddt' not in st.session_state:
-    st.session_state['eddt'] = 0
-
-
 uploaded_file = st.file_uploader('Upload xlsx file')
+if uploaded_file == None:
+    st.stop()
+
 df=pd.read_excel(uploaded_file, dtype={"Date": object})
 
-# return the dtype of each column
-result = df.dtypes
-
-# Print the result
-# st.write(result)
-
-df['Date'] = pd.to_datetime(df['Date'], format="%Y-%m-%d")
-# df.reset_index(inplace=True)
-# df = df.set_index(['Date'])
-
-st.dataframe(df)
-
-# return the dtype of each column
-result = df.dtypes
-
-# Print the result
-# st.write(result)
+a = df['Stocks'].unique()
+a=sorted(a)
+with st.sidebar:
+    for item in a:
+        st.write(item)
 
 
 
-# df['Date'] = pd.to_datetime(df['Date']).dt.date
-# df.index = pd.to_datetime(df.index)
+ticker = st.text_input('Stock Name')
 
-# return the dtype of each column
-# result = df.dtypes
-
-# # Print the result
-# st.write(result)
-
-col1, col2, col3 = st.columns(3)
-
-    
-
-st.write(st.session_state['stdt'])
-# if st.session_state['stdt'] != 0 & st.session_state['eddt'] != 0:
-#     st.write('set')
-#     df2=df.loc[stdt:eddt]
-#     st.dataframe(df2)
-# else:
-#     st.write('x')    
-
-
-def form_callback():
-    st.write(stdt)
-    st.write(eddt)
-
-    df2 = df.query('Date >= @stdt and Date <= @eddt')
-    # df2=df.loc[stdt:eddt]
-    st.dataframe(df2)
-
-with st.form(key='my_form'):
-    stdt=st.date_input ('start date',value=None , min_value=None , max_value=None , key=None )
-    eddt=st.date_input ('end date',value=None , min_value=None , max_value=None , key=None )
-    submit_button = st.form_submit_button(label='Submit', on_click=form_callback)
+if ticker != None:
+    st.write(ticker)
+    df3 = df.query('Stocks == @ticker')
+    st.dataframe(df3)
